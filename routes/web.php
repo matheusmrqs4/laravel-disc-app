72d890e42014c51238b8d7fb5f0b1175ca3881cc
Matheus Marques <matheusmrqs4@gmail.com>
2024-08-21 23:25:19 -0300

    feat: initiate channel implementation and create UserJoinedChannelEvent

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\GuildController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('guilds.index');
 });
 
 Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
